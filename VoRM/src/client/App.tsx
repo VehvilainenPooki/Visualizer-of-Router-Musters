d8b08381d3b2c6e6fdf88c29bd9e8dad3d60d981
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2026-02-23 21:46:51 +0200

    Fix public/vorm.svg import so that it won't cause issue when building

--- VoRM/src/client/App.tsx
+++ VoRM/src/client/App.tsx
@@ -2,14 +2,12 @@ import type {FC} from 'react'
 
 import Graph from './Components/Graph'
 import Sidebar from './Components/GraphManager'
-import vorm from '../../public/vorm.svg'
 
 const App: FC = () => {
   return (
     <div>
-      
       <h1 style={{ fontSize: '48px' }}>
-        <img src={vorm} alt="" style={{ height: '1.2em', verticalAlign: 'top', marginRight: '10px' }} />
+        <img src="/vorm.svg" alt="" style={{ height: '1.2em', verticalAlign: 'top', marginRight: '10px' }} />
         Visualizer of Router Musters
       </h1>
       <Sidebar />
