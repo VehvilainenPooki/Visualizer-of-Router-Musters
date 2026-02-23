9ea6f20f8d77f6fc8c7f26d49e3a6a67787b3ceb
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2026-02-23 21:05:27 +0200

    Move types.ts to src/common/types

--- VoRM/src/client/ForceGraph/simulation.ts
+++ VoRM/src/client/ForceGraph/simulation.ts
@@ -1,6 +1,6 @@
 import * as d3 from 'd3'
 
-import type { NetworkGraphData, NetworkSimulation } from '../types'
+import type { NetworkGraphData, NetworkSimulation } from '../../common/types/network'
 
 const data: NetworkGraphData = {
   nodes: [],
