e1d82e090d7db0811697bfdffcf5c1f56715ff23
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2026-02-23 17:45:30 +0200

    Add logo

--- VoRM/src/App.tsx
+++ VoRM/src/App.tsx
@@ -2,11 +2,16 @@ import type {FC} from 'react'
 
 import Graph from './Components/Graph'
 import Sidebar from './Components/GraphManager'
+import vorm from '../public/vorm.svg'
 
 const App: FC = () => {
   return (
     <div>
-      <h1>Force Graph Visualization</h1>
+      
+      <h1 style={{ fontSize: '48px' }}>
+        <img src={vorm} alt="" style={{ height: '1.2em', verticalAlign: 'top', marginRight: '10px' }} />
+        Visualizer of Router Musters
+      </h1>
       <Sidebar />
       <Graph />
     </div>
