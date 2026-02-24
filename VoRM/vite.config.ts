0829c0c0311e33e20873fa4930e3b0fd5c69d44d
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2026-02-25 00:51:35 +0200

    Fix to get Windows to propagate updates to the container for vite and nodemon --watch to see

--- VoRM/vite.config.ts
+++ VoRM/vite.config.ts
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    watch: {
+      usePolling: true,
+    },
     host: true,
     port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false
       }
     }
   }
