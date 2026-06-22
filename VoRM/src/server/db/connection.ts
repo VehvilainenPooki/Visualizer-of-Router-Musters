cdfc2c673e9d6e733ca46b3dfa6c3a4e690a652a
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2026-06-22 17:27:57 +0300

    test dokploy prod database connection

--- VoRM/src/server/db/connection.ts
+++ VoRM/src/server/db/connection.ts
@@ -6,16 +6,8 @@ import path from 'path'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://postgres:postgres@db:5432/postgres'
-const inProduction = process.env.IN_PRODUCTION === 'true'
 
-export const sequelize = new Sequelize(DATABASE_URL, {
-  dialectOptions: inProduction ? {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  } : {},
-})
+export const sequelize = new Sequelize(DATABASE_URL)
 
 export const runMigrations = async () => {
   const migrator = new Umzug({
