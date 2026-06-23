ad7110b589f597e825f1772318795a119e8aff33
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2026-06-23 15:42:52 +0300

    Remove debug logs. Dokploy issue solved. Some random service desync.

--- VoRM/src/server/index.ts
+++ VoRM/src/server/index.ts
@@ -14,7 +14,6 @@ app.use(express.json())
 
 if (inProduction) {
   console.log(`PRODUCTION running on port ${PORT}`)
-  console.log(process.env)
   app.use(express.static(path.join(process.cwd(), 'build/src/client')))
 }
 
