6093d985a642ce27927174cf408833474a6f02f0
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2026-07-01 17:59:16 +0300

    Add Mantine UI Library

--- VoRM/src/client/routes/__root.tsx
+++ VoRM/src/client/routes/__root.tsx
@@ -1,10 +1,17 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { MantineProvider } from '@mantine/core'
+import { Notifications } from '@mantine/notifications'
+import '@mantine/core/styles.css'
+import '@mantine/notifications/styles.css'
 import { AuthProvider } from '../contexts/AuthContext'
 
 export const Route = createRootRoute({
   component: () => (
-    <AuthProvider>
-      <Outlet />
-    </AuthProvider>
+    <MantineProvider>
+      <Notifications />
+      <AuthProvider>
+        <Outlet />
+      </AuthProvider>
+    </MantineProvider>
   )
 })
