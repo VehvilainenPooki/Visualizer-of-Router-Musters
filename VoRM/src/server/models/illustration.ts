deb141fa29a13b6797e442a16ee7285fee2b5678
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2026-07-14 15:23:11 +0300

    Update models to reflect new schema

--- VoRM/src/server/models/illustration.ts
+++ VoRM/src/server/models/illustration.ts
@@ -5,6 +5,9 @@ import { sequelize } from '../db/connection.js'
 class Illustration extends Model<InferAttributes<Illustration>, InferCreationAttributes<Illustration>> {
   declare id: CreationOptional<number>
   declare userId: number
+  declare name: CreationOptional<string>
+  declare description: string | null
+  declare graphcode: CreationOptional<object>
 }
 
 Illustration.init({
@@ -16,6 +19,20 @@ Illustration.init({
   userId: {
     type: DataTypes.INTEGER,
     allowNull: false
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: 'Untitled'
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  graphcode: {
+    type: DataTypes.JSONB,
+    allowNull: false,
+    defaultValue: {}
   }
 }, {
   sequelize,
