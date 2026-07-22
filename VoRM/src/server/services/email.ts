3b4bfc9e871564dd2f7912d3618063e28ad799f9
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2026-07-22 10:16:15 +0300

    Change requireTLS to be bool for mailpit

--- VoRM/src/server/services/email.ts
+++ VoRM/src/server/services/email.ts
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
   secure: false,
-  requireTLS: true,
+  requireTLS: process.env.SMTP_REQ_TLS === 'true',
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
