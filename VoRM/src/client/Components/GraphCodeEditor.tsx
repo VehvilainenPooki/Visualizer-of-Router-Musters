4ef4a07fe21e51021528bb70462b453f4299e68a
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2026-08-08 12:40:02 +0300

    Remove border radius from left side of code editor

--- VoRM/src/client/Components/GraphCodeEditor.tsx
+++ VoRM/src/client/Components/GraphCodeEditor.tsx
@@ -7,6 +7,8 @@ import CodeMirror from '@uiw/react-codemirror';
 
 
 export default function GraphCodeEditor({ editorWidth }: { editorWidth: number }) {
+  const [value, setValue] = useState("");
+
   useEffect(() => {
     setValue(JSON.stringify(ForceGraph.getData(), null, 2))
     ForceGraph.subscribe(() => {
@@ -14,13 +16,21 @@ export default function GraphCodeEditor({ editorWidth }: { editorWidth: number }
     })
   }, [])
 
-  const [value, setValue] = useState("console.log('hello world!');");
   const onChange = useCallback((val:any, _viewUpdate:any) => {
     console.log('val:', val);
     setValue(val);
   }, []);
   return (
-    <Paper style={{boxShadow:'var(--shadow-even-xs)', width: `${editorWidth}%`, height: '100%', minWidth: 0, minHeight: 0, overflow: 'hidden'}}>
+    <Paper style={{
+      boxShadow:'var(--shadow-even-xs)',
+      width: `${editorWidth}%`,
+      height: '100%', minWidth: 0,
+      minHeight: 0,
+      overflow: 'hidden',
+      position: 'relative',
+      zIndex: 1,
+      borderRadius: '0 var(--mantine-radius-default) var(--mantine-radius-default) 0'
+      }}>
       <CodeMirror value={value} height='100%' style={{ height: '100%', overflow: 'auto' }} onChange={onChange} />
     </Paper>
   )
